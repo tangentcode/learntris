fec51d531947d0735706538c7da70c999a516348
x <x@x.(none)>
2013-02-25 04:02:59 -0500

    set up all tetronimo shapes

--- Board.h
+++ Board.h
@@ -5,9 +5,11 @@ class Board
         Board();
 
         enum {BLOC_NULL, BLOC_FILL, BLOC_SIDE};
-        int boardState[12][25];
+        int boardState[25][12];
         bool getSpace(int x);
         int test = 4;
+        void initBoard();
+        void printBoard();
 };
 
 Board::Board()
@@ -15,6 +17,22 @@ Board::Board()
     memset(boardState, BLOC_NULL, sizeof(boardState));
 }
 
+void Board::initBoard()
+{
+    memset(boardState, BLOC_NULL, sizeof(boardState));
+}
+
+void Board::printBoard()
+{
+    for (int k=0; k< 300; k++)
+    {
+        std::cout << *((*boardState) + k);
+        if ((k+1)%12 == 0)
+        {
+        std::cout << std::endl;
+        }
+    }
+}
 bool Board::getSpace(int x)
 {
     if (*((*boardState) + x) != BLOC_NULL)
--- Tetro.h
+++ Tetro.h
@@ -1,26 +1,26 @@
 class Tetro
 {
     public:
-        Tetro();
         int location;
         void moveDown();
         void moveLeft();
         void moveRight();
         void rotateCounter();
         void rotateClock();
-        int shapes[4][4];
+        //Z,L,O,S,I,J,T
+        int shapes[7][16] = {{-13,-12,0,1,-11,0,1,12,-1,0,12,13,-12,-1,0,11},
+        {-12,0,12,13,-1,0,1,11,-11,-12,0,12,-11,-1,0,1},
+        {-12,-11,-1,0,-12,-11,-1,0,-12,-11,-1,0,-12,-11,-1,0},
+        {-12,-11,-1,0,-12,0,1,13,0,1,11,12,-11,-1,0,12},
+        {-23,-11,1,13,10,11,12,13,-25,-13,11,23,-14,-12,-11,-10},
+        {-12,0,11,12,-13,-1,0,1,-12,-11,0,12,-1,0,1,13},
+        {-12,-1,0,1,-12,0,1,12,-1,0,1,12,-12,-1,0,12}};
 
 };
 
-Tetro::Tetro()
-{
-
-}
-
 class Zblock : public Tetro
 {
-    public:
-    Zblock();
+
 };
 class Lblock : public Tetro
 {
@@ -45,9 +45,4 @@ class Jblock : public Tetro
 class Tblock : public Tetro
 {
 
-};
-Zblock::Zblock()
-{
-    int shape_init[16] = {-13,-12,0,1,-11,0,1,12,-13,-12,0,1,-11,0,1,12,};
-    memcpy(shapes, shape_init, sizeof(shape_init));
 };
