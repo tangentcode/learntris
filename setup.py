c424efcefe36a53fe8f8f56ba11243905f5aa5d5
tangentstorm <michal.wallace@gmail.com>
2013-04-03 09:52:05 -0500

    Added setup.py and placeholder package for tests.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='learntris',
+    version='',
+    packages=['ltclient', 'ltclient.tests'],
+    url='https://github.com/LearnProgramming/learntris',
+    license='MIT License',
+    author='tangentstorm',
+    author_email='',
+    description=''
+)
