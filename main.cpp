62e26b90457f46a0c0dcad151d038fcd28259be3
bolcselo <frozsak@gmail.com>
2013-04-04 23:18:01 -0400

    more context

--- main.cpp
+++ main.cpp
@@ -9,14 +9,7 @@ int main (int argc, char *argv[])
     tetro.current_location_y = 20;
 
     tetro.moveTetro('d',board);
-    for(int i = 0; i < 4; i++)
-    {
-        for(int j = 0; j < 2; j++)
-        {
-            std::cout << tetro.current_tetro[i][j];
-        }
-        std::cout << std::endl;
-    }
+
     board.printBoard();
 
 
