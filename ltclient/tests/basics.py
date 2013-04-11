18bd22d31d67e95e53ef0dba7da4d2474e22ceeb
tangentstorm <michal.wallace@gmail.com>
2013-04-11 01:55:45 -0500

    added some docstrings for the tests.

--- ltclient/tests/basics.py
+++ ltclient/tests/basics.py
@@ -5,10 +5,14 @@ import time, unittest
 from ltclient import client
 
 class BiosTest(unittest.TestCase):
+
     def setUp(self):
         self.bios = client.connect()
         
     def test_q(self):
+        """
+        q : should quit the system
+        """
         self.bios.send('q')
         for i in range(10):
             if self.bios.connected: time.sleep(0.1)
@@ -16,8 +20,12 @@ class BiosTest(unittest.TestCase):
                     "Disconnect failed. Did you implement the 'q' command?")
 
     def test_p(self):
+        """
+        p : should print the current game state
+        """
         self.bios.send('p')
         self.assertEquals(['. . . . . . . . . .'] * 22, self.bios.next())
+
 
 if __name__=="__main__":
     unittest.main()
