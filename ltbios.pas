a32f3398e083f98338d292c1cb7b3c7c1f6eaf47
tangentstorm <michal.wallace@gmail.com>
2013-04-11 03:04:53 -0500

    reference implementation for g(iven) command

--- ltbios.pas
+++ ltbios.pas
@@ -2,25 +2,51 @@
   (reference implementation in object pascal) }
 program ltbios;
 
-procedure print;
-  var y, x : integer;
+const
+  xmax =  9;
+  ymax = 21;
+var
+  grid : array[ 0..xmax, 0..ymax ] of char;
+  y, x : integer;
+
+procedure init;
 begin
-  for y := 22 downto 1 do begin
-    for x := 1 to 10 do write('. ');
+  for y := ymax downto 0 do
+    for x := 0 to xmax do grid[ x, y ] := '.';
+end;
+
+procedure print;
+begin
+  for y := ymax downto 0 do begin
+    for x := 0 to xmax do write( grid[ x, y ], ' ' );
     writeln;
-    flush(output);
   end
+end;
+
+procedure given;
+  var space : char;
+begin
+  for y := ymax downto 0 do begin
+    for x := 0 to xmax - 1 do begin
+      read( grid[ x, y ] );
+      read( space );
+    end;
+    read( grid[ xmax, y ] );
+    readln;
+  end;
 end;
 
 var
   cmd  : string;
   done : boolean = false;
 begin
+  init;
   repeat
     write( '> ' ); readln( cmd );
     case cmd[1] of
       'q' : done := true;
       'p' : print;
+      'g' : given;
       else
     end
   until done
