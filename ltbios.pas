dd1b3d11ec7071d71e63ccc15503ae2d83fab909
tangentstorm <michal.wallace@gmail.com>
2013-04-11 01:46:06 -0500

    reference implementation for p command.

--- ltbios.pas
+++ ltbios.pas
@@ -2,6 +2,16 @@
   (reference implementation in object pascal) }
 program ltbios;
 
+procedure print;
+  var y, x : integer;
+begin
+  for y := 22 downto 1 do begin
+    for x := 1 to 10 do write('. ');
+    writeln;
+    flush(output);
+  end
+end;
+
 var
   cmd  : string;
   done : boolean = false;
@@ -10,6 +20,7 @@ begin
     write( '> ' ); readln( cmd );
     case cmd[1] of
       'q' : done := true;
+      'p' : print;
       else
     end
   until done
